ea516d22e3493a5ba7507fdddaa91a638ba68da6
Daniel Welch <dwelch2102@gmail.com>
2018-05-12 09:43:36 -0400

    moving to zigbee2mqtt's new data path via env var approach

--- zigbee2mqtt/run.sh
+++ zigbee2mqtt/run.sh
@@ -2,23 +2,6 @@
 
 CONFIG_PATH=/data/options.json
 
-# modify the configuration.yaml based on the config passed to this add-on
+DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
 
-homeassistant=$(jq --raw-output ".homeassistant" $CONFIG_PATH)
-permit_join=$(jq --raw-output ".permit_join" $CONFIG_PATH)
-mqtt_base_topic=$(jq --raw-output ".mqtt_base_topic" $CONFIG_PATH)
-mqtt_server=$(jq --raw-output ".mqtt_server" $CONFIG_PATH)
-serial_port=$(jq --raw-output ".serial_port" $CONFIG_PATH)
-
-mqtt_user=$(jq --raw-output ".mqtt_user //empty" $CONFIG_PATH)
-mqtt_pass=$(jq --raw-output ".mqtt_pass //empty" $CONFIG_PATH)
-
-if [[ -z "$mqtt_user" ]]; then
-    echo "[Info] Setting configuration with MQTT user and password..."
-    python3 /app/set_config.py --homeassistant "$homeassistant" --permit_join "$permit_join" --mqtt_base_topic "$mqtt_base_topic" --mqtt_server "$mqtt_server" --serial_port "$serial_port" --mqtt_user="$mqtt_user" --mqtt_pass="$mqtt_pass"
-else
-    echo "[Info] Setting configuration.yaml..."
-    python3 /app/set_config.py --homeassistant "$homeassistant" --permit_join "$permit_join" --mqtt_base_topic "$mqtt_base_topic" --mqtt_server "$mqtt_server" --serial_port "$serial_port"
-fi
-
-npm start
+ZIGBEE2MQTT_DATA=${DATA_PATH} npm start
