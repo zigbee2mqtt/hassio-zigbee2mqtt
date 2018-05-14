ca05cc821a8fbc8084eadb4d7e6ef739fe22d19a
Daniel Welch <dwelch2102@gmail.com>
2018-05-14 09:57:38 -0400

    adding back in configuration via UI

--- zigbee2mqtt/run.sh
+++ zigbee2mqtt/run.sh
@@ -4,4 +4,6 @@ CONFIG_PATH=/data/options.json
 
 DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
 
+python3 --options_path ${CONFIG_PATH} --data_path ${DATA_PATH}
+
 ZIGBEE2MQTT_DATA=${DATA_PATH} npm start
