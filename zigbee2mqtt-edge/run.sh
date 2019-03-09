dc3a79fd16b99b5f61f1033d60b143c462799fea
Daniel Welch <dwelch2102@gmail.com>
2019-03-08 20:28:00 -0600

    create config backup on startup if one doesn't exist; travis changes; remove old python files

--- zigbee2mqtt-edge/run.sh
+++ zigbee2mqtt-edge/run.sh
@@ -4,15 +4,26 @@ CONFIG_PATH=/data/options.json
 
 DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
 
-mkdir -p $DATA_PATH
+# Check if config exists already
+if [[ ! -f $DATA_PATH/configuration.yaml ]]; then
+    if [[ ! -f $DATA_PATH/.configuration.yaml.bk ]]; then
+        echo "[Info] Configuration file found in data path, but no backup file found in data path. Backing up existing configuration to ${DATA_PATH}/.configuration.yaml.bk"
+        cp $DATA_PATH/configuration.yaml $DATA_PATH/.configuration.yaml.bk
+    else
+        "[Info] Configuration backup found in ${DATA_PATH}/.configuration.yaml.bk. Skipping config backup."
+    fi
+fi
+
+mkdir -p "$DATA_PATH"
 
 # Parse config
-cat $CONFIG_PATH | jq 'del(.data_path)' > $DATA_PATH/configuration.yaml
+cat "$CONFIG_PATH" | jq 'del(.data_path)' > $DATA_PATH/configuration.yaml
 
 ZIGBEE_SHEPHERD_DEBUG=$(jq --raw-output ".zigbee_shepherd_debug // empty" $CONFIG_PATH)
 ZIGBEE_SHEPHERD_DEVICES=$(jq --raw-output ".zigbee_shepherd_devices // empty" $CONFIG_PATH)
 
 if [[ ! -z "$ZIGBEE_SHEPHERD_DEBUG" ]]; then
+    echo "[Info] Zigbee Shepherd debug logging enabled."
     export DEBUG="zigbee-shepherd*"
 fi
 
