83c877c6f8bef1b5525357e6940a54bc4ceee4ab
lovelylain <jobmailcn@gmail.com>
2020-09-09 03:18:35 +0800

    add external_converters option and support more options (#353)
    * add external_converters option
    
    * support more option

--- zigbee2mqtt/run.sh
+++ zigbee2mqtt/run.sh
@@ -22,6 +22,7 @@ fi
 cat "$CONFIG_PATH" | jq 'del(.data_path, .zigbee_shepherd_debug, .zigbee_shepherd_devices, .socat)' \
     | jq 'if .advanced.ext_pan_id_string then .advanced.ext_pan_id = (.advanced.ext_pan_id_string | (split(",")|map(tonumber))) | del(.advanced.ext_pan_id_string) else . end' \
     | jq 'if .advanced.network_key_string then .advanced.network_key = (.advanced.network_key_string | (split(",")|map(tonumber))) | del(.advanced.network_key_string) else . end' \
+    | jq 'if .device_options_string then .device_options = (.device_options_string|fromjson) | del(.device_options_string) else . end' \
     > $DATA_PATH/configuration.yaml
 
 if [[ ! -z "$ZIGBEE_SHEPHERD_DEBUG" ]]|| [[ ! -z "$ZIGBEE_HERDSMAN_DEBUG" ]]; then
