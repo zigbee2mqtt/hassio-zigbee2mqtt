276d7a04d55fa3d3e5de7f3318a426380f7002fc
Marius Ciotlos <ciotlos@gmail.com>
2020-10-11 11:27:38 +0300

    Migrated to proper s6-overlay

--- common/rootfs/etc/cont-init.d/socat.sh
+++ common/rootfs/etc/cont-init.d/socat.sh
@@ -1,37 +1,19 @@
 #!/usr/bin/with-contenv bashio
-# Configuration and variables
-export DATA_PATH=$(bashio::config 'data_path')
-export SOCAT_ENABLED=$(bashio::config 'socat.enabled')
-export SOCAT_MASTER=$(bashio::config 'socat.master')
-export SOCAT_SLAVE=$(bashio::config 'socat.slave')
-export SOCAT_OPTIONS=$(bashio::config 'socat.options')
-export SOCAT_INITDELAY=$(bashio::config 'socat.initialdelay')
-export SOCAT_RESTDELAY=$(bashio::config 'socat.restartdelay')
-export SOCAT_LOG=$(bashio::config 'socat.log')
+SOCAT_MASTER=$(bashio::config 'socat.master')
+SOCAT_SLAVE=$(bashio::config 'socat.slave')
 
-# Validate input
-if [[ -z "$SOCAT_MASTER" ]]; then
-  bashio::exit.nok "Socat is enabled but not started because no master address specified"
-fi
-if [[ -z "$SOCAT_SLAVE" ]]; then
-  bashio::exit.nok "Socat is enabled but not started because no slave address specified"
-fi
-if (("$SOCAT_RESTDELAY" <= "0")); then
-  SOCAT_RESTDELAY=0
-fi
-if (("$SOCAT_INITDELAY" <= "0")); then
-  SOCAT_INITDELAY=0
-fi
-if [[ ! -z "$SOCAT_LOG" ]] && [[ "$SOCAT_LOG" = true ]]; then
-  SOCAT_LOGFILE="$DATA_PATH/socat.log"
+bashio::log.debug "Check if socat is required"
+if bashio::config.true 'socat.enabled'; then
+    # Validate input
+    if [[ -z "$SOCAT_MASTER" ]]; then
+    bashio::exit.nok "Socat is enabled but not started because no master address specified"
+    fi
+    if [[ -z "$SOCAT_SLAVE" ]]; then
+    bashio::exit.nok "Socat is enabled but not started because no slave address specified"
+    fi
 else
-  SOCAT_LOGFILE=""
+    bashio::log.info "Socat not enabled, marking service as down"
+    touch /etc/services.d/socat/down
 fi
 
-# Socat start configuration
-bashio::log.blue "Socat startup parameters:"
-bashio::log.blue "Options:     $SOCAT_OPTIONS"
-bashio::log.blue "Master:      $SOCAT_MASTER"
-bashio::log.blue "Slave:       $SOCAT_SLAVE"
-bashio::log.blue "Logfile:     $SOCAT_LOGFILE"
-bashio::log.blue "Retrydelay:  $SOCAT_RESTDELAY seconds"
+    
