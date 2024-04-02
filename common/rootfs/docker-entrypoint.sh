95e0ff71d248123e04abe76dc44889d22f68756a
Koen Kanters <koenkanters94@gmail.com>
2024-04-02 22:36:10 +0200

    Remove `zigbee_herdsman_debug` option (#587)

--- common/rootfs/docker-entrypoint.sh
+++ common/rootfs/docker-entrypoint.sh
@@ -59,11 +59,6 @@ advanced:
 EOF
 fi
 
-if bashio::config.true 'zigbee_herdsman_debug'; then
-    bashio::log.info "Zigbee Herdsman debug logging enabled"
-    export DEBUG="zigbee-herdsman:*"
-    export NODE_OPTIONS=--trace-warnings
-fi
 export NODE_PATH=/app/node_modules
 export ZIGBEE2MQTT_CONFIG_FRONTEND='{"port": 8099}'
 
