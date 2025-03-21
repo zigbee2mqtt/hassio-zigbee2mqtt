a4a66787fde2b75c9d0432bc44512aa94de3e018
Koen Kanters <koenkanters94@gmail.com>
2025-03-21 21:29:43 +0100

    Temp: disable onboarding https://github.com/Koenkk/zigbee2mqtt/pull/26608

--- common/rootfs/docker-entrypoint.sh
+++ common/rootfs/docker-entrypoint.sh
@@ -68,6 +68,7 @@ fi
 
 export NODE_PATH=/app/node_modules
 export ZIGBEE2MQTT_CONFIG_FRONTEND='{"enabled":true,"port": 8099}'
+export Z2M_ONBOARD_NO_SERVER="1"
 
 if bashio::config.true 'disable_tuya_default_response'; then
     bashio::log.info "Disabling TuYa default responses"
